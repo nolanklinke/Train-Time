46574e8df9b21da1fcd3298b7c2d0b091246fc4f
disconaked <nolan.klinke@gmail.com>
2018-06-30 17:47:36 -0400

    firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,43 @@
-// Initialize Firebase
+  // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyAhm_LupLg3-56IrgESXGPaVoAwsBoFZQc",
-    authDomain: "choochootrain-3d518.firebaseapp.com",
-    databaseURL: "https://choochootrain-3d518.firebaseio.com",
-    projectId: "choochootrain-3d518",
+    apiKey: "AIzaSyAfRX6s1QYA7kanNmlFpz-Xx3cv7e8jxHs",
+    authDomain: "choochootrain-3e164.firebaseapp.com",
+    databaseURL: "https://choochootrain-3e164.firebaseio.com",
+    projectId: "choochootrain-3e164",
     storageBucket: "",
-    messagingSenderId: "477999568922"
+    messagingSenderId: "353889985911"
   };
-
   firebase.initializeApp(config);
 
-  var databse = firebase.database();
+  var database = firebase.database();
 
   var trainName = "";
   var trainDestination = "";
   var trainTime = "";
   var trainFrequency = "";
+
+  
+$("#submitBTN").on("click", function() {
+    event.preventDefault();
+
+    trainName = $("#name-input").val().trim();
+    trainDestination = $("#destination-input").val().trim();
+    trainTime = $("#time-input").val().trim();
+    trainFrequency = $("#frequency-input").val().trim();
+
+    database.ref().push({
+        trainName: trainName,
+        trainDestination: trainDestination,
+        trainTime: trainTime,
+        trainFrequency: trainFrequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
+
+
+
+});
